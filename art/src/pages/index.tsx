a892256d20e5b20b1e2f049d36cdf0e1d78b2971
eyazeddini <eya.zeddini22@gmail.com>
2023-03-01 16:24:13 +0100

    e

--- art/src/pages/index.tsx
+++ art/src/pages/index.tsx
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,10 +21,35 @@ const Home: React.FC<MyProps> = ({ text }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className="">
-        <h1>ART is the work of god</h1>
-        <img className={styles.art1} src="/art1.jpg" alt="" srcset="" />
+      <main id={styles.body}>
+        <div >
+        <div id={styles.main}>
+        <div id={styles.box1}></div>
+
+        <div id={styles.box2}>
+            <div id={styles.text}>
+            ART is the <br/>work of god
+
+            </div>
+        </div>
+        <div id={styles.box3}>
+            <div id={styles.container}>
+              <div id={styles.logo}>
+                <b>ART</b>
+              </div>  
+              <div id={styles.menu}>
+                <ul>
+                    <li>HOME</li>
+                    <li>SERVICES</li>
+                    <li>ABOUT</li>
+                    <li>CONTACT</li>
+                    <Link href="/login"><li>login</li></Link>
+                </ul>
+              </div>
+            </div>
+        </div>
+      
+        </div>
         </div>
       </main>
     </>
